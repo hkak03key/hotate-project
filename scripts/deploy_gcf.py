a17af6c6b13186e5b94a0df0410a871b65ea40ef
Hikaru <36913018+hkak03key@users.noreply.github.com>
2020-02-18 18:39:47 +0900

    Dev/#40 (#44)
    * deploy_gcy.py: for set-iam-policy
    
    * rename gcf.yml to config.yml

--- scripts/deploy_gcf.py
+++ scripts/deploy_gcf.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
+"""
+Usage:
+    deploy_gcf.py -c <config_file> -p <policy_file> [<args>...]
+
+Options:
+    -c <config_file>, --config-file <config_file>   configファイルを設定します。
+    -p <policy_file>, --policy-file <policy_file>   iam-policyファイルを設定します。
+"""
 
 import sys
 import yaml
 import pathlib
 import subprocess
+from docopt import docopt
 
 def read_yaml(file_path):
     f = open(file_path, "r+")
@@ -34,20 +43,32 @@ def conv_dict_values(dic):
             ) for k, v in dic.items()
         }
 
-def create_cmd_array(name, dic, other_args):
+def create_deploy_cmd_array(name, dic, other_args):
     ret_origin = [["gcloud", "functions", "deploy", name]]
     ret_origin.extend([ ["--{}".format(k)] if v == True else ["--{}".format(k), v] for k, v in dic.items()])
     ret_origin.extend(other_args)
     return sum(ret_origin, [])
 
-args = sys.argv
-args.pop(0)
-conf_filepath = args.pop(-1)
+args = docopt(__doc__)
+
+conf_filepath = args["--config-file"]
 conf_filedir = pathlib.Path(conf_filepath).resolve().parent
 name = conf_filedir.name
+other_args = args["<args>"]
 
-cmd_array = create_cmd_array(name, conv_dict_values(replace_source_path(drop_false(read_yaml(conf_filepath)), conf_filedir)), args)
+config_params = read_yaml(conf_filepath)
+deploy_cmd_array = create_deploy_cmd_array(name, conv_dict_values(replace_source_path(drop_false(config_params), conf_filedir)), other_args)
 
-print("exec:", " ".join(cmd_array))
-exit(subprocess.call(cmd_array))
+print("exec:", " ".join(deploy_cmd_array))
+ret = subprocess.call(deploy_cmd_array)
+
+if ret != 0:
+    exit(ret)
+
+policy_filepath = args["--policy-file"]
+set_iam_policy_cmd_array = ["gcloud", "functions", "set-iam-policy", name, "--region", config_params["region"], policy_filepath]
+
+print("exec:", " ".join(set_iam_policy_cmd_array))
+ret = subprocess.call(set_iam_policy_cmd_array)
+exit(ret)
 
