c7ed3d5f9f46e5e7f738894fa17ed35973d90eb6
Hikaru <36913018+hkak03key@users.noreply.github.com>
2020-02-26 18:31:02 +0900

    dev/#68: secret for twitter (#69)

--- terraform/modules/resources/google_secret_manager.tf
+++ terraform/modules/resources/google_secret_manager.tf
@@ -1,9 +1,19 @@
 
 
-resource "google_secret_manager_secret_iam_binding" "editor" {
+resource "google_secret_manager_secret_iam_binding" "LINE" {
   provider  = google-beta
   project   = var.project
   secret_id = "LINE"
+  role      = "roles/secretmanager.secretAccessor"
+  members = [
+    "serviceAccount:${var.project}@appspot.gserviceaccount.com",
+  ]
+}
+
+resource "google_secret_manager_secret_iam_binding" "twitter" {
+  provider  = google-beta
+  project   = var.project
+  secret_id = "twitter"
   role      = "roles/secretmanager.secretAccessor"
   members = [
     "serviceAccount:${var.project}@appspot.gserviceaccount.com",
