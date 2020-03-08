c726f4117e58d0c60b2bdc6affbd27ee6fe51ed5
Hikaru <36913018+hkak03key@users.noreply.github.com>
2020-03-09 00:25:50 +0900

    dev/#86: mv and mod google_cloud_scheduler.tf (#110)

---
+++ terraform/modules/resources/google_cloud_scheduler.tf
@@ -0,0 +1,51 @@
+
+resource "google_cloud_scheduler_job" "call_gcf_today_attendance" {
+  # 本番環境のみschedulerを作成
+  for_each = { for v in(var.project == "hotate-project" ? ["call_gcf_today_attendance"] : []) :
+    v => v
+  }
+  name             = each.key
+  description      = "call gcf today_attendance"
+  schedule         = "0 17 * * *"
+  time_zone        = "Asia/Tokyo"
+  attempt_deadline = "120s"
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://${var.region}-${var.project}.cloudfunctions.net/today_attendance"
+
+    oidc_token {
+      service_account_email = "${var.project}@appspot.gserviceaccount.com"
+    }
+  }
+
+  depends_on = [
+    google_project_service.project_service["cloudscheduler.googleapis.com"],
+  ]
+}
+
+resource "google_cloud_scheduler_job" "call_gcf_schedule_update_info" {
+  # 本番環境のみschedulerを作成
+  for_each = { for v in(var.project == "hotate-project" ? ["call_gcf_schedule_update_info"] : []) :
+    v => v
+  }
+  name             = each.key
+  description      = "call gcf schedule_update_info"
+  schedule         = "30 08-23/7 * * *"
+  time_zone        = "Asia/Tokyo"
+  attempt_deadline = "180s"
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://${var.region}-${var.project}.cloudfunctions.net/schedule_update_info"
+
+    oidc_token {
+      service_account_email = "${var.project}@appspot.gserviceaccount.com"
+    }
+  }
+
+  depends_on = [
+    google_project_service.project_service["cloudscheduler.googleapis.com"],
+  ]
+}
+
