f26d6e769f09f26948a2658fcd376a6c6c6a1f14
Hikaru <hkak03key@gmail.com>
2020-03-12 11:03:59 +0900

    dev/#107 create-backend-resources: mod deploy user role

--- scripts/create-backend-resources.sh
+++ scripts/create-backend-resources.sh
@@ -11,7 +11,7 @@ gsutil mb -p $PJ_NAME -l us-west1 gs://$BUCKET_NAME/
 gcloud services enable iam.googleapis.com # 実行ユーザが所属するprojectのserviceを有効にする必要がある
 
 gcloud iam service-accounts create $SA_NAME --display-name $SA_NAME --project $PJ_NAME
-gcloud projects add-iam-policy-binding $PJ_NAME --member serviceAccount:$SA_FULLNAME --role roles/editor
+gcloud projects add-iam-policy-binding $PJ_NAME --member serviceAccount:$SA_FULLNAME --role roles/owner
 gcloud iam service-accounts keys create `cd $(dirname $0) && pwd`/../terraform/gcp_service_key.json --iam-account $SA_FULLNAME --project $PJ_NAME
 
 # cloudresourcemanager.googleapis.comは手動で有効にする必要がある
